c55b89bdd2391ff14a806aa6a7571cd5bc72a090
Alex-Curran <alexcurran839@gmail.com>
2015-06-07 20:57:32 -0500

    Test for Robot Part Deliever

---
+++ RobotFactoryTests/RobotSupplierTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using RobotFactory;
+
+namespace RobotFactoryTests
+{
+    [TestFixture]
+    internal class RobotSupplierTests
+    {
+        [Test]
+        public void RobotPartSupplierDeliversRobotParts()
+        {
+            var mechSupplier = new RobotPartSupplier
+            {
+                RobotParts = new List<RobotPart> {
+                    new MockedRobotPart(),
+                    new MockedRobotPart()
+                }
+            };
+
+            var deliveryBay = new MockedDeliveryBay();
+            mechSupplier.DeliverRobotParts(deliveryBay);
+
+            Assert.AreEqual(2, deliveryBay.RobotParts.Count);
+            Assert.AreEqual(0, mechSupplier.RobotParts.Count);
+        }
+    }
+}
